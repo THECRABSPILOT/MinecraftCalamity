b8e1bae50cbcf41cfc2c75adfab1c2b0e5ce25e7
THECRABSPILOT <vbcjrmoore.13@gmail.com>
2026-07-22 18:55:48 -0700

    Start of Accessory system, added gui textures

---
+++ src/main/java/crab/mods/minecraftcalamity/client/KeyBindings.java
@@ -0,0 +1,57 @@
+package crab.mods.minecraftcalamity.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import crab.mods.minecraftcalamity.MinecraftCalamity;
+import crab.mods.minecraftcalamity.network.ModMessages;
+import crab.mods.minecraftcalamity.network.OpenAccessoryMenuC2SPacket;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBindings {
+
+    public static final String KEY_CATEGORY = "key.categories." + MinecraftCalamity.MODID;
+    public static final String KEY_OPEN_ACCESSORIES = "key." + MinecraftCalamity.MODID + ".open_accessories";
+
+    public static final KeyMapping ACCESSORY_KEY = new KeyMapping(
+            KEY_OPEN_ACCESSORIES,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_P,
+            KEY_CATEGORY
+    );
+
+    @Mod.EventBusSubscriber(modid = MinecraftCalamity.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ModClientEvents {
+        @SubscribeEvent
+        public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
+            event.register(ACCESSORY_KEY);
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MinecraftCalamity.MODID, value = Dist.CLIENT)
+    public static class ForgeClientEvents {
+        @SubscribeEvent
+        public static void onKeyInput(TickEvent.ClientTickEvent event) {
+            if (event.phase == TickEvent.Phase.END) {
+                while (ACCESSORY_KEY.consumeClick()) {
+                    if (Minecraft.getInstance().player != null) {
+
+                        // 1. Send chat message to player (Client-side)
+                        Minecraft.getInstance().player.sendSystemMessage(
+                                Component.literal("Opening Accessory Menu...")
+                        );
+
+                        // 2. Send packet to server to open the screen
+                        ModMessages.sendToServer(new OpenAccessoryMenuC2SPacket());
+                    }
+                }
+            }
+        }
+    }
+}
